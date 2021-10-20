e60ab32b82c061ac81db308c7d175184af9a27e9
qintianchen <qintianchen@bytedance.com>
2021-10-20 22:20:38 +0800

    feat:Voronoi [3]

--- Assets/Scripts/Voronoi/DCEL.cs
+++ Assets/Scripts/Voronoi/DCEL.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Text;
 using UnityEngine;
 
 namespace VoronoiEngine
@@ -15,28 +16,59 @@ namespace VoronoiEngine
             edgeList = new List<DCELHalfEdge>();
             vertexList = new List<DCELVertex>();
         }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append($"Total faces: {faceList.Count}, total edges: {edgeList.Count}, total vertices: {vertexList.Count}\n");
+            foreach (var f in faceList)
+            {
+                var e = f.edge;
+                sb.Append($"{e.ori}, {e.suc.ori}, {e.suc.suc.ori}\n");
+            }
+
+            return sb.ToString();
+        }
     }
 
     public class DCELFace
     {
         public DCELHalfEdge edge;
+        
+        public bool ContainsPosition(DCELPosition position)
+        {
+            // Only if the point is to the same side (all to the right or all to the left)
+            // of all three edges does that mean the face contains the point
+            var e1 = edge;
+            var e2 = e1.suc;
+            var e3 = e2.suc;
+            var res1 = e1.GetPositionOrientation(position);
+            var res2 = e2.GetPositionOrientation(position);
+            var res3 = e3.GetPositionOrientation(position);
+            return (res1 < 0 && res2 < 0 && res3 < 0) || (res1 > 0 && res2 > 0 && res3 > 0);
+        }
     }
     
     public class DCELHalfEdge
     {
-        public DCELVertex head;
-        public DCELVertex tail;
+        public DCELVertex ori; // The starting point of the edge
 
-        public DCELHalfEdge twinEdge;
+        public DCELHalfEdge twin;
         public DCELFace face;
 
-        public DCELHalfEdge preEdge;
-        public DCELHalfEdge sucEdge;
-
-        public DCELHalfEdge(DCELVertex head, DCELVertex tail)
+        public DCELHalfEdge pre;
+        public DCELHalfEdge suc;
+        
+        /// <returns>return positive for left, negative for right</returns>
+        public float GetPositionOrientation(DCELPosition position)
         {
-            this.head = head;
-            this.tail = tail;
+            float x1 = ori.position.x;
+            float y1 = ori.position.y;
+            float x2 = suc.ori.position.x;
+            float y2 = suc.ori.position.y;
+            float x = position.x;
+            float y = position.y;
+            return x1 * (y2 - y) + y1 * (x - x2) + x2 * y - x * y2;
         }
     }
     
@@ -48,6 +80,11 @@ namespace VoronoiEngine
         public DCELVertex(float x, float y)
         {
             position = new DCELPosition(x, y);
+        }
+
+        public override string ToString()
+        {
+            return $"({position.x}, {position.y})";
         }
     }
 
--- Assets/Scripts/Voronoi/Voronoi.cs
+++ Assets/Scripts/Voronoi/Voronoi.cs
@@ -8,7 +8,7 @@ namespace VoronoiEngine
     {
         public List<DCELPosition> unCheckedPoints;
         private DCEL dcel;
-        
+
         public Voronoi()
         {
             dcel = new DCEL();
@@ -38,29 +38,29 @@ namespace VoronoiEngine
             DCELVertex v0 = new DCELVertex(superTriangle[0].x, superTriangle[0].y);
             DCELVertex v1 = new DCELVertex(superTriangle[1].x, superTriangle[1].y);
             DCELVertex v2 = new DCELVertex(superTriangle[2].x, superTriangle[2].y);
-            DCELHalfEdge e01 = new DCELHalfEdge(v0, v1);
-            DCELHalfEdge e12 = new DCELHalfEdge(v1, v2);
-            DCELHalfEdge e20 = new DCELHalfEdge(v2, v0);
-            DCELHalfEdge e10 = new DCELHalfEdge(v1, v0);
-            DCELHalfEdge e21 = new DCELHalfEdge(v2, v1);
-            DCELHalfEdge e02 = new DCELHalfEdge(v0, v2);
-            e01.twinEdge = e10;
-            e10.twinEdge = e01;
-            e12.twinEdge = e21;
-            e21.twinEdge = e12;
-            e20.twinEdge = e02;
-            e02.twinEdge = e20;
+            DCELHalfEdge e01 = new DCELHalfEdge{ori = v0};
+            DCELHalfEdge e12 = new DCELHalfEdge{ori = v1};
+            DCELHalfEdge e20 = new DCELHalfEdge{ori = v2};
+            DCELHalfEdge e10 = new DCELHalfEdge{ori = v1};
+            DCELHalfEdge e21 = new DCELHalfEdge{ori = v2};
+            DCELHalfEdge e02 = new DCELHalfEdge{ori = v0};
+            e01.twin = e10;
+            e10.twin = e01;
+            e12.twin = e21;
+            e21.twin = e12;
+            e20.twin = e02;
+            e02.twin = e20;
             DCELFace superFace = new DCELFace();
             superFace.edge = e01;
             v0.edge = e01;
             v1.edge = e12;
             v2.edge = e20;
-            e01.preEdge = e20;
-            e01.sucEdge = e12;
-            e12.preEdge = e01;
-            e12.sucEdge = e20;
-            e20.preEdge = e12;
-            e20.sucEdge = e01;
+            e01.pre = e20;
+            e01.suc = e12;
+            e12.pre = e01;
+            e12.suc = e20;
+            e20.pre = e12;
+            e20.suc = e01;
             e01.face = superFace;
             e12.face = superFace;
             e20.face = superFace;
@@ -75,26 +75,193 @@ namespace VoronoiEngine
             dcel.edgeList.Add(e02);
             dcel.faceList.Add(superFace);
 
-            // SECOND: choose one point to add to the List first
-            var firstPoint = unCheckedPoints[0];
-            unCheckedPoints.Remove(firstPoint);
-            
-        }
-        
-        private void ConnectPointsToCreateTriangles(Point center, Point[] ps)
-        {
-            if (ps.Length < 2) return;
-            
-            for (var i = 0; i < ps.Length-1; i++)
+            // SECOND: choose one point to add to the List
+            while (unCheckedPoints.Count > 0)
             {
-                Point[] triPoints = new Point[3];
-                triPoints[0] = center;
-                triPoints[1] = ps[i];
-                triPoints[2] = ps[i+1];
-                // triangles.Add(new Triangle(triPoints));    
+                var point = unCheckedPoints[unCheckedPoints.Count - 1];
+                unCheckedPoints.RemoveAt(unCheckedPoints.Count - 1);
+                AddPointToDelaunay(point, out var unCheckedEdges, out var newVertex);
+                foreach (var edge in unCheckedEdges)
+                {
+                    CheckEdge(edge, newVertex);
+                }
             }
         }
 
+        private void CheckEdge(DCELHalfEdge edge, DCELVertex vertex)
+        {
+            var e = edge;
+            var p = vertex;
+            var p1 = e.ori;
+            var p2 = e.suc.ori;
+            var p0 = e.twin.pre.ori;
+
+            var x1 = p.position.x;
+            var y1 = p.position.y;
+            var x2 = p1.position.x;
+            var y2 = p1.position.y;
+            var x3 = p2.position.x;
+            var y3 = p2.position.y;
+            var x4 = p0.position.x;
+            var y4 = p0.position.y;
+
+            // positive means outside of the circumcircle, negative means inside
+            var res = MCMath.Determinant(new[]
+            {
+                new[] {x1, y1, x1 * x1 + y1 * y1, 1},
+                new[] {x2, y2, x2 * x2 + y2 * y2, 1},
+                new[] {x3, y3, x3 * x3 + y3 * y3, 1},
+                new[] {x4, y4, x4 * x4 + y4 * y4, 1}
+            });
+
+            if (res <= 0)
+            {
+                // dcel.edgeList.Remove(edge);
+                FlipEdge(edge, out var newEdges);
+                foreach (var newEdge in newEdges)
+                {
+                    if (newEdge.ori != vertex && newEdge.suc.ori != vertex)
+                    {
+                        CheckEdge(newEdge, vertex);
+                    }
+                }
+            }
+
+        }
+
+        private void AddPointToDelaunay(DCELPosition position, out DCELHalfEdge[] unCheckedEdges, out DCELVertex newVertex)
+        {
+            // todo: Check if the point lies on one the edges
+            newVertex = new DCELVertex(position.x, position.y);
+            dcel.vertexList.Add(newVertex);
+            
+            // Add d point to delaunay will lead to one of the surfaces to tear, we have to check which face it is
+            DCELFace f1 = GetFaceContainsPoint(position);
+
+            DCELHalfEdge e1 = f1.edge;
+            DCELHalfEdge e2 = e1.suc;
+            DCELHalfEdge e3 = e2.suc;
+            
+            // 1. tear the face
+            DCELFace f2 = new DCELFace();
+            DCELFace f3 = new DCELFace();
+            f2.edge = f1.edge.suc;
+            f3.edge = f2.edge.suc;
+            dcel.faceList.Add(f2);
+            dcel.faceList.Add(f3);
+            
+            DCELVertex p1 = f1.edge.ori;
+            DCELVertex p2 = f1.edge.suc.ori;
+            DCELVertex p3 = f1.edge.suc.suc.ori;
+
+            DCELHalfEdge e4 = new DCELHalfEdge();
+            DCELHalfEdge e5 = new DCELHalfEdge();
+            DCELHalfEdge e6 = new DCELHalfEdge();
+            DCELHalfEdge e7 = new DCELHalfEdge();
+            DCELHalfEdge e8 = new DCELHalfEdge();
+            DCELHalfEdge e9 = new DCELHalfEdge();
+            
+            dcel.edgeList.Add(e4);
+            dcel.edgeList.Add(e5);
+            dcel.edgeList.Add(e6);
+            dcel.edgeList.Add(e7);
+            dcel.edgeList.Add(e8);
+            dcel.edgeList.Add(e9);
+
+            e4.ori = p2;
+            e5.ori = newVertex;
+            e6.ori = newVertex;
+            e7.ori = p1;
+            e8.ori = p3;
+            e9.ori = newVertex;
+
+            e4.suc = e6;
+            e4.pre = e1;
+            e5.suc = e2;
+            e5.pre = e8;
+            e6.suc = e1;
+            e6.pre = e4;
+            e7.suc = e9;
+            e7.pre = e3;
+            e8.suc = e5;
+            e8.pre = e2;
+            e9.suc = e3;
+            e9.pre = e7;
+
+            e4.twin = e5;
+            e5.twin = e4;
+            e6.twin = e7;
+            e7.twin = e6;
+            e8.twin = e9;
+            e9.twin = e8;
+
+            e2.face = f2;
+            e3.face = f3;
+            f2.edge = e2;
+            f3.edge = e3;
+
+            newVertex.edge = e6;
+
+            unCheckedEdges = new[] {e1, e2, e3};
+        }
+
+        private void FlipEdge(DCELHalfEdge e, out DCELHalfEdge[] edges)
+        {
+            var e1 = e.suc;
+            var e2 = e.suc.suc;
+            var e3 = e.twin.suc;
+            var e4 = e.twin.suc.suc;
+
+            var p1 = e.ori;
+            var p2 = e.suc.ori;
+            var p3 = e.suc.suc.ori;
+            var p4 = e3.suc.ori;
+
+            var f1 = e.face;
+            var f2 = e.twin.face;
+
+            f1.edge = e;
+            f2.edge = e.twin;
+
+            p1.edge = e3;
+            p2.edge = e1;
+            p3.edge = e2;
+            p4.edge = e4;
+
+            e1.face = f2;
+            e2.face = f1;
+            e3.face = f1;
+            e4.face = f2;
+
+            e1.pre = e4;
+            e1.suc = e.twin;
+            e2.pre = e;
+            e2.suc = e3;
+            e3.pre = e2;
+            e3.suc = e;
+            e4.pre = e.twin;
+            e4.suc = e1;
+            e.pre = e3;
+            e.suc = e2;
+            e.twin.pre = e1;
+            e.twin.suc = e4;
+
+            edges = new[] {e1, e2, e3, e4};
+        }
+
+        private DCELFace GetFaceContainsPoint(DCELPosition position)
+        {
+            foreach (var dcelFace in dcel.faceList)
+            {
+                if (dcelFace.ContainsPosition(position))
+                {
+                    return dcelFace;
+                }
+            }
+
+            return null;
+        }
+        
         // Get a super triangle from given collection of Point
         private DCELPosition[] GetSuperTriangle(IEnumerable<DCELPosition> _points)
         {
